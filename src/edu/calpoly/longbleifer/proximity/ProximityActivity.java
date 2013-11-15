34c8a5f99cf9ffb5867d4f2f033d26b92770152f
Robert <robert@robertlong.me>
2013-11-15 00:37:43 -0800

    Fixed beacon service and added notifications.

--- src/edu/calpoly/longbleifer/proximity/ProximityActivity.java
+++ src/edu/calpoly/longbleifer/proximity/ProximityActivity.java
@@ -64,14 +64,8 @@ public class ProximityActivity extends FragmentActivity {
         	setMainContent(0);
         }
         
-        Calendar cal = Calendar.getInstance();
-
         Intent intent = new Intent(this, BeaconService.class);
-        PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
-
-        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        // Start every 30 seconds
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pintent);
+        startService(intent);
         
 	}
 	
